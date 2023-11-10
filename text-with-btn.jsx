94ecd3cd0c9a0c01931a0dcd779b17c870a4bd9e
searchsakib <91392414+searchsakib@users.noreply.github.com>
2023-11-11 04:04:51 +0600

    text-with-btn
    Text with btn react component for empty pages or any other needs.

---
+++ text-with-btn.jsx
@@ -0,0 +1,13 @@
+<div className="grid col-span-2 gap-10 mt-10 px-10 py-10">
+        <h2 className="text-3xl text-center font-medium">
+          You haven't posted any Jobs Yet...
+        </h2>
+        <Link
+          to="/"
+          rel="noopener noreferrer"
+          href="#"
+          className="px-8 py-3 font-semibold rounded bg-[#05386B] text-white hover:outline  hover:bg-white hover:text-[#05386B] text-center w-1/3 mx-auto"
+        >
+          Back to homepage
+        </Link>
+      </div>
