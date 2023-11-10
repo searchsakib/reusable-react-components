462b0d422ded44a8e7bb0ebb883b392e00ffec9d
searchsakib <91392414+searchsakib@users.noreply.github.com>
2023-11-11 04:33:01 +0600

    dummy text added

--- text-with-btn.jsx
+++ text-with-btn.jsx
@@ -2,7 +2,7 @@
 
 <div className="grid grid-cols-1 gap-10 mt-10 px-10 py-10">
         <h2 className="text-3xl text-center font-medium">
-          You haven't posted any Jobs Yet...
+          This is dummy text...
         </h2>
         <Link
           to="/"
@@ -21,7 +21,7 @@
 
 <div className="grid col-span-2 gap-10 mt-10 px-10">
             <h2 className="text-3xl text-center font-medium">
-              You haven't posted any Jobs Yet...
+              This is dummy text...
             </h2>
             <Link
               to="/"
