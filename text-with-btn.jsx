037836388b5b89f0e49e5bcaaf0c70da3bc3ac3e
searchsakib <91392414+searchsakib@users.noreply.github.com>
2023-11-11 04:25:46 +0600

    2 kinds of code

--- text-with-btn.jsx
+++ text-with-btn.jsx
@@ -1,4 +1,6 @@
- <div className="grid grid-cols-1 gap-10 mt-10 px-10 py-10">
+// Just general use
+
+<div className="grid grid-cols-1 gap-10 mt-10 px-10 py-10">
         <h2 className="text-3xl text-center font-medium">
           You haven't posted any Jobs Yet...
         </h2>
@@ -11,3 +13,22 @@
           Back to homepage
         </Link>
       </div>
+
+
+
+
+// if this code used within a parent div with grid:
+
+<div className="grid col-span-2 gap-10 mt-10 px-10">
+            <h2 className="text-3xl text-center font-medium">
+              You haven't posted any Jobs Yet...
+            </h2>
+            <Link
+              to="/"
+              rel="noopener noreferrer"
+              href="#"
+              className="px-8 py-3 font-semibold rounded bg-[#05386B] text-white hover:outline  hover:bg-white hover:text-[#05386B] text-center lg:w-1/3 mx-auto"
+            >
+              Back to homepage
+            </Link>
+          </div>
